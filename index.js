61dcd6b8f3338cd11c8bb51864e560cc092bc373
Valer <valerghahramanyan@gmail.com>
2020-09-30 16:58:29 +0400

    first-commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const fs = require('fs')
+const app = express();
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+let usersCounter = 0
+let usersData = {}
+app.set("view engine", "ejs")
+app.use("/public", express.static("public"))
+app.listen(8080)
+let log = false;
+// fs.truncate('./auth/auth.json', 0, function(){console.log('data is cleaned')})
+
+app.get("/", (req, res) => {
+    res.render("login")
+})
+
+app.get("/authFailed", (req, res) => {
+    res.render("errorPages/authFailed")
+})
+app.get("/registration", (req, res) => {
+    res.render("register")
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+app.post('/login', urlencodedParser, (req, rws) => {
+    const user = { ...req.body }
+    usersData[`user_${++usersCounter}`] = user
+    fs.writeFile('./auth/auth.json', JSON.stringify(usersData), 'utf8', () => {
+        console.log(user);
+    });
+    rws.render('login')
+})
+app.post("/", urlencodedParser, (req, res) => {
+    const auth = fs.readFileSync('./auth/auth.json', { "Context-Type": "application/json; charset=utf-8" }, (err) => {
+        console.error(err)
+    })
+    const jsonAuth = JSON.parse(auth)
+    const sendedInfo = req.body
+
+    for (let i in jsonAuth) {
+        const user = jsonAuth[i]
+        if (user.email === sendedInfo.email && user.password === sendedInfo.password) {
+            log = true;
+            if (log) res.render('index', { data: user, logged: log })
+            return;
+        }
+    }
+    res.render("errorPages/authFailed")
+})
